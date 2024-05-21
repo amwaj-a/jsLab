20eff07459e979d8b455ac5836144729fd49036b
Amwaj <amwaj.1431@outlook.com>
2024-05-21 18:28:52 +0300

    v1

---
+++ main.js
@@ -0,0 +1,40 @@
+let hieght = 12;
+let width = 10;
+let area = hieght * width;
+
+console.log(area);
+
+let tem = 37;
+let f = tem * 2 + 30;
+console.log(`${tem}C is ${f}F`);
+
+let c = (f - 30) / 2;
+console.log(`${f}C is ${c}F`);
+
+let hour = 5;
+let second = hour * 3600;
+console.log(`${hour} hours = ${second} Seconds`);
+
+thingOne = "Hello";
+thingTwo = "world";
+console.log(thingOne + " " + thingTwo);
+
+// let bill = 10.25 + 3.99 + 7.15, tip=0.15  , total= bill* tip;
+var bill = 10.25;
+bill = bill + 3.99 + 7.15;
+var tip = 0.15,
+  total = bill * tip;
+console.log(total);
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+let madLib = `The Intro to JavaScript course is  ${adjective1}. James and Julia are so  ${adjective2}. I cannot wait to work through the rest of this  ${adjective3} content!`;
+
+console.log(madLib);
+
+firstName = "Amwaj ";
+interest = " Reading";
+hobby = " Programming";
+awesomeMessage = `
+Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby}.`;
+console.log(awesomeMessage);
